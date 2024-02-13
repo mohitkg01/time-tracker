21260e702e3e63c8d956f74ddf23120044bc27e2
Mohit Kumar Gond <109984401+mohitkg01@users.noreply.github.com>
2024-02-13 13:04:04 +0530

    Update TrackerCoreConfig.java

--- core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
+++ core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
@@ -11,12 +11,12 @@ import java.util.List;
 /**
  * Created by Jason on 6/19/2015.
  */
-@Configuration
-@ComponentScan("training.taylor.timetracker.core")
-public class TrackerCoreConfig {
+// @Configuration
+// @ComponentScan("training.taylor.timetracker.core")
+// public class TrackerCoreConfig {
 
-    @Bean(name = "timesheet")
-    public List<TimeEntry> timeEntries() {
-        return new ArrayList<>();
-    }
-}
+//     @Bean(name = "timesheet")
+//     public List<TimeEntry> timeEntries() {
+//         return new ArrayList<>();
+//     }
+// }
